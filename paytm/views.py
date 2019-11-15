547fbe87de68e58bd52918e944bd3f7682b737b7
Ayush Aggarwal <aggarwalayush775@gmail.com>
2019-11-15 23:42:13 +0530

    working commit. Paytm integration upto orders page

--- paytm/views.py
+++ paytm/views.py
@@ -16,50 +16,76 @@ def home(request):
     return HttpResponse("<html><a href='"+ settings.HOST_URL +"/paytm/payment'>PayNow</html>")
 
 
-def payment(request):
-    try:
-        print('4---------', request.user)
-        MERCHANT_KEY = settings.PAYTM_MERCHANT_KEY
-        MERCHANT_ID = settings.PAYTM_MERCHANT_ID
-        get_lang = "/" + get_language() if get_language() else ''
-        CALLBACK_URL = settings.HOST_URL + settings.PAYTM_CALLBACK_URL
-        # Generating unique temporary ids
-        order_id = Checksum.__id_generator__()
+# def payment(request):
+#     try:
+#         print('4---------', request.user)
+#         MERCHANT_KEY = settings.PAYTM_MERCHANT_KEY
+#         MERCHANT_ID = settings.PAYTM_MERCHANT_ID
+#         get_lang = "/" + get_language() if get_language() else ''
+#         CALLBACK_URL = settings.HOST_URL + settings.PAYTM_CALLBACK_URL
+#         # Generating unique temporary ids
+#         order_id = Checksum.__id_generator__()
 
-        bill_amount = 50
-        if bill_amount:
-            # data_dict = {
-            #             'MID':"BUfEWY28036097078675",
-            #             'ORDER_ID':str(order_id),
-            #             'TXN_AMOUNT':str(bill_amount),
-            #             'CUST_ID':'harih@pickrr.com',
-            #             'INDUSTRY_TYPE_ID':'Retail',
-            #             'WEBSITE':settings.PAYTM_WEBSITE,
-            #             'CHANNEL_ID':'WEB',
-            #             'CALLBACK_URL':'http://localhost:8000/paytm/response',
-            #         }
+#         bill_amount = 50
+#         if bill_amount:
+#             # data_dict = {
+#             #             'MID':"BUfEWY28036097078675",
+#             #             'ORDER_ID':str(order_id),
+#             #             'TXN_AMOUNT':str(bill_amount),
+#             #             'CUST_ID':'harih@pickrr.com',
+#             #             'INDUSTRY_TYPE_ID':'Retail',
+#             #             'WEBSITE':settings.PAYTM_WEBSITE,
+#             #             'CHANNEL_ID':'WEB',
+#             #             'CALLBACK_URL':'http://localhost:8000/paytm/response',
+#             #         }
 
-            data_dict = {
+#             data_dict = {
 
-                'MID': MERCHANT_ID,
-                'ORDER_ID': str(order_id),
-                'TXN_AMOUNT': str(10),
-                'CUST_ID': ' ',
-                'INDUSTRY_TYPE_ID': 'Retail',
-                'WEBSITE': 'WEBSTAGING',
-                'CHANNEL_ID': 'WEB',
-                'CALLBACK_URL':'http://127.0.0.1:8000/paytm/response/',
+#                 'MID': MERCHANT_ID,
+#                 'ORDER_ID': str(67),
+#                 'TXN_AMOUNT': str(100),
+#                 'CUST_ID': 'vipulshri4@gmail.com',
+#                 'INDUSTRY_TYPE_ID': 'Retail',
+#                 'WEBSITE': 'WEBSTAGING',
+#                 'CHANNEL_ID': 'WEB',
+#                 'CALLBACK_URL':'http://127.0.0.1:8000/paytm/response/',
 
-        }
+#         }
             
-            param_dict = data_dict        
-            param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
-            print(param_dict['CHECKSUMHASH'])
-            return render(request,"payment.html",{'paytmdict':param_dict})
-        return HttpResponse("Bill Amount Could not find. ?bill_amount=10")
-    except Exception as e:
-        print('error in paytm payment function', str(e))
-        return HttpResponse(str(e))
+#             param_dict = data_dict        
+#             param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
+#             print(param_dict['CHECKSUMHASH'])
+#             return render(request,"payment.html",{'paytmdict':param_dict})
+#         return HttpResponse("Bill Amount Could not find. ?bill_amount=10")
+#     except Exception as e:
+#         print('error in paytm payment function', str(e))
+#         return HttpResponse(str(e))
+
+
+def payment(request):
+    MERCHANT_KEY = settings.PAYTM_MERCHANT_KEY
+    MERCHANT_ID = settings.PAYTM_MERCHANT_ID
+    CALLBACK_URL = settings.HOST_URL+ settings.PAYTM_CALLBACK_URL
+    # Generating unique temporary ids
+    order_id = Checksum.__id_generator__()
+
+    bill_amount = 100
+    if bill_amount:
+        data_dict = {
+                    'MID':MERCHANT_ID,
+                    'ORDER_ID':str(order_id),
+                    'MOBILE_NO': '7777777777',
+                    'TXN_AMOUNT': str(bill_amount),
+                    'CUST_ID':'eieojiojoi@gmail.com',
+                    'INDUSTRY_TYPE_ID':'Retail',
+                    'WEBSITE': 'WEBSTAGING',
+                    'CHANNEL_ID':'WEB',
+                    'CALLBACK_URL': CALLBACK_URL,
+                }
+        param_dict = data_dict
+        param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(data_dict, MERCHANT_KEY)
+        return render(request,"payment.html",{'paytmdict':param_dict})
+    return HttpResponse("Bill Amount Could not find. ?bill_amount=10")
 
 @csrf_exempt
 def response(request):
